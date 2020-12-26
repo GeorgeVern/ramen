838d5213ea4a297305278069c0c14018f7657345
giorgos <giwrgos_412@hotmail.com>
2020-12-26 17:22:49 +0100

    changes to ramen code

--- code/alignment/example_mono.sh
+++ code/alignment/example_mono.sh
@@ -1,25 +1,36 @@
 # runing example for en-vi
 set -x
 VEC_PATH=../../data/vecs
-ALIGN=../tools/fastText/alignment/align.py
+ALIGN=/home/georgios.vernikos/UMT/tools/fastText/alignment/align.py
 VOCAB_PATH=../../data/bert/vocab
 PROB_PATH=../../data/bert/probs
+TGT_VOCAB=/home/georgios.vernikos/workspace/LMMT/MonoEgo/spanish-tokenizer_lc/vocab.txt
 
 mkdir -p $PROB_PATH
 N=100000
-for lg in en vi; do
-  wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.${lg}.300.vec.gz -P $VEC_PATH
-  gunzip $VEC_PATH/cc.${lg}.300.vec.gz
-  # get wordlist
-  head -$N $VEC_PATH/cc.$lg.300.vec > $VEC_PATH/cc.$lg.300.vec.$N
-  cut -f1 -d ' ' $VEC_PATH/cc.$lg.300.vec.$N > $VEC_PATH/wordlist.$lg
+for lg in en es; do
+  if ! [[ -f "$VEC_PATH/cc.$lg.300.vec.$N" ]]; then
+    wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.${lg}.300.vec.gz -P $VEC_PATH
+    gunzip $VEC_PATH/cc.${lg}.300.vec.gz
+    # get wordlist
+    head -$N $VEC_PATH/cc.$lg.300.vec > $VEC_PATH/cc.$lg.300.vec.$N
+  fi
+  if ! [[ -f "$VEC_PATH/wordlist.$lg" ]]; then
+    cut -f1 -d ' ' $VEC_PATH/cc.$lg.300.vec.$N > $VEC_PATH/wordlist.$lg
+  fi
 done
 
-lg=vi
+lg=es
 
-# note that it's important to  have wordlist.en as the first argument
-python get_common_words.py $VEC_PATH/wordlist.en $VEC_PATH/wordlist.$lg > $VEC_PATH/wordlist.en-$lg
+if ! [[ -f "$VEC_PATH/wordlist.en-$lg" ]]; then
+  # note that it's important to  have wordlist.en as the first argument
+  python get_common_words.py $VEC_PATH/wordlist.en $VEC_PATH/wordlist.$lg > $VEC_PATH/wordlist.en-$lg
+fi
 
-python $ALIGN --src_emb $VEC_PATH/cc.$lg.300.vec --tgt_emb $VEC_PATH/cc.en.300.vec --dico_train $VEC_PATH/wordlist.en-${lg} --dico_test $VEC_PATH/wordlist.en-$lg --output $VEC_PATH/aligned.$lg.vec  --niter 10 --maxload $N
+if ! [[ -f "$VEC_PATH/aligned.$lg.vec" ]]; then
+  python $ALIGN --src_emb $VEC_PATH/cc.$lg.300.vec --tgt_emb $VEC_PATH/cc.en.300.vec --dico_train $VEC_PATH/wordlist.en-${lg} --dico_test $VEC_PATH/wordlist.en-$lg --output $VEC_PATH/aligned.$lg.vec  --niter 10 --maxload $N
+fi
 
-python get_prob_vect.py --src_aligned_vec $VEC_PATH/cc.en.300.vec --src_vocab $VOCAB_PATH/bert-base-cased-vocab.txt --topn 50000 --tgt_aligned_vec $VEC_PATH/aligned.$lg.vec --tgt_vocab $VOCAB_PATH/$lg-vocab.txt --save $PROB_PATH/probs.mono.en-$lg.pth
+if ! [[ -f "PROB_PATH/probs.mono.en-$lg.pth" ]]; then
+  python get_prob_vect.py --src_aligned_vec $VEC_PATH/cc.en.300.vec --src_vocab $VOCAB_PATH/bert-base-uncased-vocab.txt --topn 50000 --tgt_aligned_vec $VEC_PATH/aligned.$lg.vec --tgt_vocab $TGT_VOCAB --save $PROB_PATH/probs.mono.en-$lg.pth
+fi
--- code/alignment/get_prob_vect.py
+++ code/alignment/get_prob_vect.py
@@ -3,7 +3,7 @@
 import argparse
 import torch
 from collections import defaultdict
-from transformers import BertTokenizer
+from transformers import BertTokenizer, AutoTokenizer, BertTokenizerFast
 import numpy as np
 from sparsemax import Sparsemax
 import io
@@ -12,17 +12,17 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser(
     description="Estimate word translation probability from aligned fastText vectors")
 
-parser.add_argument('--src_aligned_vec', default='wiki.en.align.vec',
+parser.add_argument('--src_aligned_vec', default='../../data/vecs/cc.en.300.vec',
                     help="source embeddings in aligned space.")
-parser.add_argument('--tgt_aligned_vec', default='',
+parser.add_argument('--tgt_aligned_vec', default='../../data/vecs/aligned.es.vec',
                     help="source embeddings in aligned space.")
 parser.add_argument('--src_vocab', default='',
                     help='path to src vocabulary')
-parser.add_argument('--tgt_vocab', default='',
+parser.add_argument('--tgt_vocab', default='/home/georgios.vernikos/workspace/LMMT/MonoEgo/spanish-tokenizer_lc/vocab.txt',
                     help="target vocabulary file.")
 parser.add_argument('--topn', type=int, default=100000,
                     help="max number of words in the vocab")
-parser.add_argument('--save', default='', help='path to save file')
+parser.add_argument('--save', default='../../data/bert/probs/probs.mono.en-es.pth', help='path to save file')
 
 
 def load_vec(fname, nmax=50000):
@@ -33,10 +33,11 @@ def load_vec(fname, nmax=50000):
         next(f)
         for i, line in enumerate(f):
             word, vect = line.rstrip().split(' ', 1)
+            word = word.lower()
             vect = np.fromstring(vect, sep=' ')
-            assert word not in word2id, 'word found twice'
-            vectors.append(vect)
-            word2id[word] = len(word2id)
+            if word not in word2id:
+                vectors.append(vect)
+                word2id[word] = len(word2id)
             if len(word2id) == nmax:
                 break
     id2word = {v: k for k, v in word2id.items()}
@@ -82,8 +83,11 @@ def renorm(x, dim):
 def main():
     params = parser.parse_args()
     print(params)
-    src_tokenizer = BertTokenizer(params.src_vocab, do_lower_case=False)
-    tgt_tokenizer = BertTokenizer(params.tgt_vocab, do_lower_case=False)
+    # src_tokenizer = BertTokenizer(params.src_vocab, do_lower_case=True)
+    src_tokenizer = AutoTokenizer.from_pretrained(
+        'bert-base-uncased', cache_dir='/home/georgios.vernikos/workspace/LMMT/MonoEgo/cache', use_fast=True
+    )
+    tgt_tokenizer = BertTokenizerFast(vocab_file=params.tgt_vocab, do_lower_case=True)
 
     src_embs, src_subwords = get_subword_embeddings(src_tokenizer, params.src_aligned_vec, params.topn)
     tgt_embs, tgt_subwords = get_subword_embeddings(tgt_tokenizer, params.tgt_aligned_vec, params.topn)
